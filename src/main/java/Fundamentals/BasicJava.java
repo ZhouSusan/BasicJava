b23b0799421c368a5e9128b756625e28d040c74b
ZhouSusan <szhou089@gmail.com>
2022-11-19 20:55:24 -0800

    added print1To255() in BasicJava.java

--- src/main/java/Fundamentals/BasicJava.java
+++ src/main/java/Fundamentals/BasicJava.java
@@ -1,12 +1,24 @@
 package Fundamentals;
 
 public class BasicJava {
+
+    //Method that prints all the odd numbers from 1 to 255.
     public static void printOddSum() {
         for (int i = 1; i <= 255; i+= 2) {
             System.out.println(i);
         }
     }
+
+    //Method that prints the numbers from 0 to 255
+    static void print1To255() {
+        int sum = 0;
+        for (int i = 0; i <= 255; i++) {
+            sum += i;
+            System.out.format("New number: %s Sum: %s\n", i, sum);
+        }
+    }
     public static void main(String[] args) {
         printOddSum();
+        print1To255();
     }
 }
