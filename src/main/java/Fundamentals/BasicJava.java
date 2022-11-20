b78f430bd160827094ecdb95da560913507a0344
ZhouSusan <szhou089@gmail.com>
2022-11-19 20:47:20 -0800

    added printOddSum() in BasicJava.java

--- src/main/java/Fundamentals/BasicJava.java
+++ src/main/java/Fundamentals/BasicJava.java
@@ -1,7 +1,12 @@
 package Fundamentals;
 
 public class BasicJava {
+    public static void printOddSum() {
+        for (int i = 1; i <= 255; i+= 2) {
+            System.out.println(i);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printOddSum();
     }
 }
