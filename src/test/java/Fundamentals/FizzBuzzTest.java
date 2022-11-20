da42b33ec027e01e7d4b9a9c6c53d2405cef5c6f
ZhouSusan <szhou089@gmail.com>
2022-11-20 03:14:33 -0800

    added void fizzBuzzTest() in FizzBuzzTest.java

--- src/test/java/Fundamentals/FizzBuzzTest.java
+++ src/test/java/Fundamentals/FizzBuzzTest.java
@@ -1,10 +1,17 @@
 package Fundamentals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static Fundamentals.FizzBuzz.fizzBuzz;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class FizzBuzzTest {
 
     @org.junit.jupiter.api.Test
-    void fizzBuzz() {
+    void fizzBuzzTest() {
+        List<String> res = new ArrayList<String>(Arrays.asList("1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"));
+        assertArrayEquals(res.toArray(), fizzBuzz(15).toArray());
     }
 }
